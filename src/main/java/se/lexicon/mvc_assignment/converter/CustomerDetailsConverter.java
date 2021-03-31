1eb6089efe560853fd0ecf367af3620a20ef00c8
Eddie <eddie.ch.89d@gmail.com>
2021-03-31 14:02:02 +0200

    Converter Details Converter

--- src/main/java/se/lexicon/mvc_assignment/converter/CustomerDetailsConverter.java
+++ src/main/java/se/lexicon/mvc_assignment/converter/CustomerDetailsConverter.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import se.lexicon.mvc_assignment.dto.CustomerDetailsDto;
 import se.lexicon.mvc_assignment.entity.CustomerDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 
@@ -14,21 +15,64 @@ public class CustomerDetailsConverter implements Converter<CustomerDetails, Cust
 
     @Override
     public CustomerDetails toModel(CustomerDetailsDto dto) {
-        return null;
+        CustomerDetails model = new CustomerDetails();
+
+        if (dto != null) {
+            model.setDetailsId(dto.getDetailsId());
+            model.setCellphone(dto.getCellphone());
+            model.setCity(dto.getCity());
+            model.setStreet(dto.getStreet());
+            model.setHomePhone(dto.getHomePhone());
+            model.setZipCode(dto.getZipCode());
+        }
+
+        return model;
     }
+
 
     @Override
     public CustomerDetailsDto toDTO(CustomerDetails model) {
-        return null;
+        CustomerDetailsDto dto = new CustomerDetailsDto();
+
+        if (model != null) {
+            dto.setDetailsId(model.getDetailsId());
+            dto.setCellphone(model.getCellphone());
+            dto.setHomePhone(model.getHomePhone());
+            dto.setCity(model.getCity());
+            dto.setStreet(model.getStreet());
+            dto.setZipCode(model.getZipCode());
+        }
+
+        return dto;
     }
+
 
     @Override
     public Collection<CustomerDetails> toModels(Collection<CustomerDetailsDto> collection) {
-        return null;
+        Collection<CustomerDetails> models= new ArrayList<>();
+
+        if (collection !=null){
+            for (CustomerDetailsDto dto: collection){
+                models.add(toModel(dto));
+            }
+        }
+
+        return models;
     }
+
 
     @Override
     public Collection<CustomerDetailsDto> toDTos(Collection<CustomerDetails> collection) {
-        return null;
+        Collection<CustomerDetailsDto> dtoList = new ArrayList<>();
+
+        if (collection !=null){
+            for (CustomerDetails model: collection){
+                dtoList.add(toDTO(model));
+            }
+        }
+
+        return dtoList;
     }
+
+
 }
