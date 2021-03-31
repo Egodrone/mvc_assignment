563de1cb4b5c68242f65e5683faee7eeafb039a6
Eddie <eddie.ch.89d@gmail.com>
2021-03-31 14:08:58 +0200

    Customer Converter

--- src/main/java/se/lexicon/mvc_assignment/converter/CustomerConverter.java
+++ src/main/java/se/lexicon/mvc_assignment/converter/CustomerConverter.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import se.lexicon.mvc_assignment.dto.CustomerDto;
 import se.lexicon.mvc_assignment.entity.Customer;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 
@@ -29,23 +30,56 @@ public class CustomerConverter implements Converter<Customer, CustomerDto> {
             model.setEmail(dto.getEmail());
             model.setRegDate(dto.getRegDate());
             model.setActive(true);
-            //model.setCustomerDetails();
+            model.setCustomerDetails(customerDetailsConverter.toModel(dto.getCustomerDetailsDto()));
         }
+
         return model;
     }
 
+
     @Override
     public CustomerDto toDTO(Customer model) {
-        return null;
+        CustomerDto dto = new CustomerDto();
+
+        if (model != null) {
+            dto.setActive(true);
+            dto.setCustomerId(model.getCustomerId());
+            dto.setEmail(model.getEmail());
+            dto.setRegDate(model.getRegDate());
+            dto.setCustomerDetailsDto(customerDetailsConverter.toDTO(model.getCustomerDetails()));
+        }
+
+        return dto;
     }
+
 
     @Override
     public Collection<Customer> toModels(Collection<CustomerDto> collection) {
-        return null;
+        Collection<Customer> models = new ArrayList<>();
+
+        if (collection != null) {
+            for (CustomerDto dto : collection) {
+                models.add(toModel(dto));
+            }
+        }
+
+        return models;
     }
+
 
     @Override
     public Collection<CustomerDto> toDTos(Collection<Customer> collection) {
-        return null;
+        Collection<CustomerDto> dtoList = new ArrayList<>();
+
+        if (collection != null) {
+            for (Customer model : collection) {
+                CustomerDto dto = toDTO(model);
+                dtoList.add(dto);
+            }
+        }
+
+        return dtoList;
     }
+
+
 }
