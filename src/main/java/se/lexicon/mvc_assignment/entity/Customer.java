3292dd85c0bbd4b413d4582afc6a0f1d31549b0c
Eddie <eddie.ch.89d@gmail.com>
2021-04-16 09:05:47 +0200

    implemented lombok

--- src/main/java/se/lexicon/mvc_assignment/entity/Customer.java
+++ src/main/java/se/lexicon/mvc_assignment/entity/Customer.java
@@ -1,15 +1,15 @@
 package se.lexicon.mvc_assignment.entity;
 
+import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.Objects;
 
 
+@Data
 @Entity
 public class Customer {
-
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(
@@ -26,88 +26,5 @@ public class Customer {
     private boolean active;
     @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private CustomerDetails customerDetails;
-
-    public Customer() {
-    }
-
-    public Customer(String email, LocalDate regDate, boolean active, CustomerDetails customerDetails) {
-        this.email = email;
-        this.regDate = regDate;
-        this.active = active;
-        this.customerDetails = customerDetails;
-    }
-
-    public Customer(String customerId, String email, LocalDate regDate, boolean active, CustomerDetails customerDetails) {
-        this.customerId = customerId;
-        this.email = email;
-        this.regDate = regDate;
-        this.active = active;
-        this.customerDetails = customerDetails;
-    }
-
-    public String getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getRegDate() {
-        return regDate;
-    }
-
-    public void setRegDate(LocalDate regDate) {
-        this.regDate = regDate;
-    }
-
-    public boolean getActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public CustomerDetails getCustomerDetails() {
-        return customerDetails;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Customer customer = (Customer) o;
-        return active == customer.active && Objects.equals(customerId, customer.customerId) && Objects.equals(email, customer.email) && Objects.equals(regDate, customer.regDate) && Objects.equals(customerDetails, customer.customerDetails);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(customerId, email, regDate, active, customerDetails);
-    }
-
-    public void setCustomerDetails(CustomerDetails customerDetails) {
-        this.customerDetails = customerDetails;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "customerId='" + customerId + '\'' +
-                ", email='" + email + '\'' +
-                ", regDate=" + regDate +
-                ", active=" + active +
-                ", customerDetails=" + customerDetails +
-                '}';
-    }
-
 
 }
--- src/main/java/se/lexicon/mvc_assignment/entity/CustomerDetails.java
+++ src/main/java/se/lexicon/mvc_assignment/entity/CustomerDetails.java
@@ -1,15 +1,16 @@
 package se.lexicon.mvc_assignment.entity;
 
 
+import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.util.Objects;
 
 
+@Data
 @Entity
 public class CustomerDetails {
     @Id
@@ -31,99 +32,5 @@ public class CustomerDetails {
     private String homePhone;
     @Column(nullable = false, length = 10)
     private String cellphone;
-
-    public CustomerDetails() {
-    }
-
-    public CustomerDetails(String street, String zipCode, String city, String homePhone, String cellphone) {
-        this.street = street;
-        this.zipCode = zipCode;
-        this.city = city;
-        this.homePhone = homePhone;
-        this.cellphone = cellphone;
-    }
-
-    public CustomerDetails(String detailsId, String street, String zipCode, String city, String homePhone, String cellphone) {
-        this.detailsId = detailsId;
-        this.street = street;
-        this.zipCode = zipCode;
-        this.city = city;
-        this.homePhone = homePhone;
-        this.cellphone = cellphone;
-    }
-
-    public String getDetailsId() {
-        return detailsId;
-    }
-
-    public void setDetailsId(String detailsId) {
-        this.detailsId = detailsId;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getHomePhone() {
-        return homePhone;
-    }
-
-    public void setHomePhone(String homePhone) {
-        this.homePhone = homePhone;
-    }
-
-    public String getCellphone() {
-        return cellphone;
-    }
-
-    public void setCellphone(String cellphone) {
-        this.cellphone = cellphone;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CustomerDetails that = (CustomerDetails) o;
-        return Objects.equals(detailsId, that.detailsId) && Objects.equals(street, that.street) && Objects.equals(zipCode, that.zipCode) && Objects.equals(city, that.city) && Objects.equals(homePhone, that.homePhone) && Objects.equals(cellphone, that.cellphone);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(detailsId, street, zipCode, city, homePhone, cellphone);
-    }
-
-    @Override
-    public String toString() {
-        return "CustomerDetails{" +
-                "detailsId='" + detailsId + '\'' +
-                ", street='" + street + '\'' +
-                ", zipCode='" + zipCode + '\'' +
-                ", city='" + city + '\'' +
-                ", homePhone='" + homePhone + '\'' +
-                ", cellphone='" + cellphone + '\'' +
-                '}';
-    }
-
 
 }
