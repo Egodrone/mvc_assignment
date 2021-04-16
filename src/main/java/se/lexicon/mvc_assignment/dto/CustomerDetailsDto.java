cfa8a052eace5b9262a41eda8ebb420bf5f9ee9d
Eddie <eddie.ch.89d@gmail.com>
2021-04-16 09:15:56 +0200

    implemented lombok

--- src/main/java/se/lexicon/mvc_assignment/dto/CustomerDetailsDto.java
+++ src/main/java/se/lexicon/mvc_assignment/dto/CustomerDetailsDto.java
@@ -1,13 +1,14 @@
 package se.lexicon.mvc_assignment.dto;
 
 
-import javax.validation.constraints.Digits;
+import lombok.Data;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Objects;
 
 
+@Data
 public class CustomerDetailsDto {
     private String detailsId;
     @NotNull(message = "Street can't be null")
@@ -30,91 +31,5 @@ public class CustomerDetailsDto {
     @NotEmpty(message = "Mobile phone should not be empty")
     @Size(min = 6, max = 10, message = "Mobile phone should be between 6 and 10 numbers")
     private String cellphone;
-
-
-    public CustomerDetailsDto() {
-    }
-
-    public CustomerDetailsDto(String detailsId, String street, String zipCode, String city, String homePhone, String cellphone) {
-        this.detailsId = detailsId;
-        this.street = street;
-        this.zipCode = zipCode;
-        this.city = city;
-        this.homePhone = homePhone;
-        this.cellphone = cellphone;
-    }
-
-    public String getDetailsId() {
-        return detailsId;
-    }
-
-    public void setDetailsId(String detailsId) {
-        this.detailsId = detailsId;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getHomePhone() {
-        return homePhone;
-    }
-
-    public void setHomePhone(String homePhone) {
-        this.homePhone = homePhone;
-    }
-
-    public String getCellphone() {
-        return cellphone;
-    }
-
-    public void setCellphone(String cellphone) {
-        this.cellphone = cellphone;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CustomerDetailsDto that = (CustomerDetailsDto) o;
-        return Objects.equals(detailsId, that.detailsId) && Objects.equals(street, that.street) && Objects.equals(zipCode, that.zipCode) && Objects.equals(city, that.city) && Objects.equals(homePhone, that.homePhone) && Objects.equals(cellphone, that.cellphone);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(detailsId, street, zipCode, city, homePhone, cellphone);
-    }
-
-    @Override
-    public String toString() {
-        return "CustomerDetailsDto{" +
-                "detailsId=" + detailsId +
-                ", street='" + street + '\'' +
-                ", zipCode='" + zipCode + '\'' +
-                ", city='" + city + '\'' +
-                ", homePhone='" + homePhone + '\'' +
-                ", cellphone='" + cellphone + '\'' +
-                '}';
-    }
 
 }
--- src/main/java/se/lexicon/mvc_assignment/dto/CustomerDto.java
+++ src/main/java/se/lexicon/mvc_assignment/dto/CustomerDto.java
@@ -1,15 +1,17 @@
 package se.lexicon.mvc_assignment.dto;
 
 
+import lombok.Data;
+
 import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
-import java.util.Objects;
 
 
+@Data
 public class CustomerDto {
     private String customerId;
     @NotNull
@@ -21,80 +23,5 @@ public class CustomerDto {
     private boolean active;
     @Valid
     private CustomerDetailsDto customerDetailsDto;
-
-    public CustomerDto() {
-    }
-
-    public CustomerDto(String customerId, String email, LocalDate regDate, boolean active, CustomerDetailsDto customerDetailsDto) {
-        this.customerId = customerId;
-        this.email = email;
-        this.regDate = regDate;
-        this.active = active;
-        this.customerDetailsDto = customerDetailsDto;
-    }
-
-    public String getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getRegDate() {
-        return regDate;
-    }
-
-    public void setRegDate(LocalDate regDate) {
-        this.regDate = regDate;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public CustomerDetailsDto getCustomerDetailsDto() {
-        return customerDetailsDto;
-    }
-
-    public void setCustomerDetailsDto(CustomerDetailsDto customerDetailsDto) {
-        this.customerDetailsDto = customerDetailsDto;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CustomerDto that = (CustomerDto) o;
-        return active == that.active && Objects.equals(customerId, that.customerId) && Objects.equals(email, that.email) && Objects.equals(regDate, that.regDate) && Objects.equals(customerDetailsDto, that.customerDetailsDto);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(customerId, email, regDate, active, customerDetailsDto);
-    }
-
-    @Override
-    public String toString() {
-        return "CustomerDto{" +
-                "customerId=" + customerId +
-                ", email='" + email + '\'' +
-                ", regDate=" + regDate +
-                ", active=" + active +
-                ", customerDetailsDto=" + customerDetailsDto +
-                '}';
-    }
 
 }
